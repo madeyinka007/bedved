4e6e728083a90de296bd0861e8e8c6714b6d14f0
madeyinka007 <mutairadeyinka@yahoo.com>
2024-05-29 03:47:29 +0100

    app update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
-import { Suspense } from "react";
-import Layout from "./pages/Layout";
 import Home from "./pages/Home";
 import { Route, Routes } from "react-router-dom";
-import Preloader from "./common/Preloader";
 import About from "./pages/About";
 import Services from "./pages/Services";
 import Blogs from "./pages/Blogs";
